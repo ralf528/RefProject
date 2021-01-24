cba299224469512c353845ef0de8cd1a8f5fbf96
ralf528 <ralf519@naver.com>
2021-01-24 10:55:59 +0900

    궁수 Move 애니메이션 추가

--- trunk/Sources/Template/TableManager.cpp
+++ trunk/Sources/Template/TableManager.cpp
@@ -50,8 +50,34 @@ void TableManager::LoadTemplate(int TemplateID)
 	}
 	else if (TemplateID == JOB_ARCHER)
 	{
+		CharacterTemplate Data;
+		Data.TemplateID = JOB_KNIGHT;
+
+		Data.SkillType_1 = E_SkillType_NormalAttack;
+		Data.SkillType_2 = E_SkillType_Dash;
+		Data.SkillType_3 = E_SkillType_ShootWhole;
+		Data.SkillType_4 = E_SkillType_Inbeatable;
+
 		// 궁수 이미지
-		//RC_MGR->addImage(imgID_WARRIOR_IDLE, "Data/Resource/Image/character/archer/archer_idle.bmp", 0, 0, RM_TRANS);
+		Data.m_AnimationDatas.insert(make_pair(imgID_WARRIOR_IDLE, AnimationTemplate{ imgID_WARRIOR_IDLE , "Data/Resource/Image/character/archer/archer_idle.bmp", SIZE{0, 0}, 8, 8, 50, true, true, true }));
+		Data.m_AnimationDatas.insert(make_pair(imgID_WARRIOR_MOVE, AnimationTemplate{ imgID_WARRIOR_MOVE , "Data/Resource/Image/character/archer/archer_walk.bmp", SIZE{0, 0}, 8, 8, 50, false, false, true }));
+		// 임시
+		Data.m_AnimationDatas.insert(make_pair(imgID_WARRIOR_ATK, AnimationTemplate{ imgID_WARRIOR_ATK  , "Data/Resource/Image/character/warrior_atk.bmp", SIZE{0, 0}, 18, 8, 20, false, false, true }));
+		Data.m_AnimationDatas.insert(make_pair(imgID_WARRIOR_DIE, AnimationTemplate{ imgID_WARRIOR_DIE  , "Data/Resource/Image/character/warrior_die.bmp", SIZE{0, 0}, 20, 8, 200, false, false, false }));
+		Data.m_AnimationDatas.insert(make_pair(imgID_WARRIOR_BEHIT, AnimationTemplate{ imgID_WARRIOR_BEHIT, "Data/Resource/Image/character/warrior-gethit.bmp", SIZE{0, 0}, 7, 8, 20, false, false, true }));
+		Data.m_AnimationDatas.insert(make_pair(imgID_WARRIOR_DASH, AnimationTemplate{ imgID_WARRIOR_DASH , "Data/Resource/Image/character/dash.bmp", SIZE{0, 0}, 5, 1, 40, false, false, true }));
+
+		for (auto& each : Data.m_AnimationDatas)
+		{
+			RC_MGR->addImage(each.first, each.second.path, 0, 0, RM_TRANS);
+			auto img = RC_MGR->findImage(each.first);
+			if (img != nullptr)
+			{
+				each.second.size = img->getSize();
+			}
+		}
+
+		m_CharacterDatas.insert(make_pair(JOB_KNIGHT, Data));
 	}
 }
 
